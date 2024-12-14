71893d27d4d68cf2133e670d31b7936598b34964
GustavoLucoli <gustavo.lucoli.koepsel@gmail.com>
2024-12-13 22:26:32 -0300

    feat: create login component

---
+++ src/app/login/page.js
@@ -0,0 +1,11 @@
+import Login from "../../components/Login/index.js";
+import Register from "../../components/Register/index.js";
+
+export default function login() {
+  return (
+    <div>
+      <Login />
+      <Register />
+    </div>
+  );
+}
