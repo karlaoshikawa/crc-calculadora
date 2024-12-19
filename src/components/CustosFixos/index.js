481c8c61e5011d39e92ebb4f785262280054b1ed
GustavoLucoli <gustavo.lucoli.koepsel@gmail.com>
2024-12-18 23:07:58 -0300

    feat:create home page components

---
+++ src/components/CustosFixos/index.js
@@ -0,0 +1,44 @@
+export default function CustosFixos() {
+  return (
+    <div>
+      <p> custos fixos</p>
+      <label htmlFor="aluguel">
+        Aluguel
+        <input type="number" id="aluguel" className="aluguel" />
+      </label>
+      <label htmlFor="energia">
+        Energia
+        <input type="number" id="energia" className="energia" />
+      </label>
+      <label htmlFor="agua">
+        Água
+        <input type="number" id="agua" className="agua" />
+      </label>
+      <label htmlFor="internet">
+        Internet
+        <input type="number" id="internet" className="internet" />
+      </label>
+      <label htmlFor="mei">
+        MEI
+        <input type="number" id="mei" className="mei" />
+      </label>
+      <label htmlFor="site">
+        site/Loja Virtual
+        <input type="number" id="site" className="site" />
+      </label>
+      <label htmlFor="telefone">
+        Telefone/Celular
+        <input type="number" id="telefone" className="telefone" />
+      </label>
+      <label htmlFor="colaboradores">
+        Colaboradores
+        <input type="number" id="colaboradores" className="colaboradores" />
+      </label>
+      <label htmlFor="outros">
+        Outros custos
+        <input type="text" id="outros" className="outros" />
+      </label>
+      <p>Total dos custos fixos</p>
+    </div>
+  );
+}
--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -3,7 +3,7 @@ export default function Register() {
     <div className="register-container">
       <span>Por favor utilizar o mesmo e-mail que fez a compra no Hotmart</span>
 
-      <label for="email">E-mail</label>
+      <label htmlFor="email">E-mail</label>
       <input
         type="email"
         className="email"
@@ -11,13 +11,14 @@ export default function Register() {
         required
       />
 
-      <label for="password">Senha</label>
+      <label htmlFor="password">Senha</label>
       <input
         type="password"
         className="password"
         placeholder="Digite sua senha aqui"
         required
       />
+      <a>Finalizar cadastro</a>
     </div>
   );
 }
