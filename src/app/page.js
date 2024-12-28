c0945648d407d52a3a20e52b989afaf5ce2ffc71
GustavoLucoli <gustavo.lucoli.koepsel@gmail.com>
2024-12-27 22:11:10 -0300

    feat:fix material  component

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import BoxInfo from "../components/BoxInfo/index.js";
 import FixedCosts from "../components/FixedCosts/index.js";
+import Material from "../components/Material/index.js";
 import Packaging from "../components/Packaging/index.js";
 import Remuneration from "../components/Remuneration/index.js";
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <div>
       <BoxInfo />
+      <Material />
       <FixedCosts />
       <Packaging />
       <Remuneration />
--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -5,6 +5,7 @@ export default function Register() {
 
       <label htmlFor="email">E-mail</label>
       <input
+        value="gustavo.lucoli.koepsel@gmail.com"
         type="email"
         className="email"
         placeholder="exemplo.email@email.com"
@@ -18,7 +19,7 @@ export default function Register() {
         placeholder="Digite sua senha aqui"
         required
       />
-      <p>Finalizar cadastro</p>
+      <p>Finalizar Cadastro</p>
     </div>
   );
 }
