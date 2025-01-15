43347d7df4af3e2d6d41d36f6321ac6728086162
GustavoLucoli <gustavo.lucoli.koepsel@gmail.com>
2025-01-14 22:09:44 -0300

    feat:modified login page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import BoxInfo from "../components/BoxInfo/index.js";
 import FixedCosts from "../components/FixedCosts/index.js";
+
 import Material from "../components/Material/index.js";
 import Packaging from "../components/Packaging/index.js";
 import Remuneration from "../components/Remuneration/index.js";
